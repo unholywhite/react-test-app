6fb2aef27c2829650aee668a6524d03b58eeef8b
Sergii Krupnyk <s.krupnyk@gmail.com>
2019-01-16 17:37:52 +0200

    trying React

---
+++ components.js
@@ -0,0 +1,11 @@
+class StoryBox extends React.Component {
+    render() {
+        return(
+          <div>Story Box</div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <StoryBox />, document.getElementById('story-app')
+);
